ade9cefd5d10533bd1101cc0602b28010a420cb2
Kringlaren <adam.kring06@gmail.com>
2025-03-28 12:10:40 +0100

    kommentarer fungerar

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -14,10 +14,12 @@ export const getAllPosts = async (req, res) => {
 
 // Skapar ett inlägg med eller utan bild
 export const createPost = async (req, res) => {
-    const { userId, textContent } = req.body;
-    const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
+    const userId = req.body;
+    const textContent = checkTextContent(req, res);
 
-    if (!textContent) return res.status(codes.BAD_REQUEST).json({ message: "Inlägget måste innehålla text!" })
+    if (!textContent) return;
+
+    const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
 
     try {
         await db.query("INSERT INTO posts (user_id, content, image) VALUES (?, ?, ?)", [userId, textContent, imageUrl]);
@@ -31,9 +33,51 @@ export const createPost = async (req, res) => {
 // Kommentarer  //
 //////////////////
 
+// Skapar kommentar på ett inlägg med eller utan bild
 export const createComment = async (req, res) => {
-    const { userId, postId, textContent } = req.body;
+    const { userId, postId } = req.body;
+    const textContent = checkTextContent(req, res);
+
+    if (!textContent) return;
+
     const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
 
-    
+    try {
+        await db.query("INSERT INTO comments (user_id, post_id, content, image) VALUES (?, ?, ?, ?)", [userId, postId, textContent, imageUrl]);
+        res.status(codes.CREATED).json({ message: "Kommentar skapad!" });
+    } catch (error) {
+        res.status(codes.SERVER_ERROR).json({ message: "Serverfel", error});
+    } 
 }
+
+// Hämtar alla kommentarer för ett visst inlägg
+export const getCommentsForPost = async (req, res) => {
+    const { postId } = req.body;
+
+    try {
+        const [rows] = await db.query("SELECT * FROM comments WHERE post_id=?", [postId]);
+        res.status(codes.OK).json(rows);
+        
+    } catch (error) {
+        res.status(codes.SERVER_ERROR).json({ message: "Serverfel", error});
+    } 
+}
+
+// Gillningar //
+////////////////
+
+
+
+// Hjälpfunktioner //
+/////////////////////
+
+const checkTextContent = (req, res) => {
+    const {textContent} = req.body;
+
+    if (!textContent) {
+        res.status(codes.BAD_REQUEST).json({ message: "Inlägget måste innehålla text!" });
+        return null;
+    }
+    return textContent;
+}
+
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createPost, getAllPosts } from "../controllers/postController.js";
+import { createPost, getAllPosts, createComment, getCommentsForPost } from "../controllers/postController.js";
 import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
+// Inlägg
 router.post('/create', upload.single("image"), createPost);
 router.get('/all', getAllPosts);
+
+// Kommentarer
+router.post('/comment', upload.single("image"), createComment);
+router.get('/comments', getCommentsForPost);
 
 export default router;
